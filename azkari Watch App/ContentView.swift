1cbd799d17c62974bd04b3e373bc24937aebef3c
Haitham Assoli <haitham.assoli@goldentik.com>
2026-07-21 17:34:21 +0300

    Initial Commit

---
+++ azkari Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  azkari Watch App
+//
+//  Created by Goldentik on 21/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ azkari Watch App/azkariApp.swift
@@ -0,0 +1,17 @@
+//
+//  azkariApp.swift
+//  azkari Watch App
+//
+//  Created by Goldentik on 21/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct azkari_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
